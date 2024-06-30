d7fd51196a8cd628c814485c9fdcb24cee3f215a
nthapa000 <nishantthapa0000@gmail.com>
2024-07-01 00:24:22 +0530

    RegisterForm

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -1,6 +1,4 @@
-// we are not exporting as default since it is NOT a page and it is just a component
 "use client";
-// since we are using hook
 import * as z from "zod";
 
 import { useForm } from "react-hook-form";
@@ -16,48 +14,46 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 
-import { LoginSchema } from "@/schemas";
+import { RegisterSchema } from "@/schemas";
 import { CardWrapper } from "./card-wrapper";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { FormError } from "../form-errors";
 import { FormSuccess } from "../form-success";
-import { login } from "@/actions/login";
+import { register } from "@/actions/register";
 
-export const LoginForm = () => {
+export const RegisterForm = () => {
   const [error,setError] = useState<string | undefined>("");
   const [success,setSuccess] = useState<string | undefined>("");
   const [isPending,startTransition]=useTransition();
 
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
     defaultValues: {
       email: "",
       password: "",
+      name:""
     },
   });
 
-  const onSubmit = (values:z.infer<typeof LoginSchema>)=>{
-    // Everytime we hit the new submit then we will clear all successs and error message
+  const onSubmit = (values:z.infer<typeof RegisterSchema>)=>{
     setError("");
     setSuccess("");
     
     startTransition(()=>{
-      login(values)
+      register(values)
         .then((data)=>{
           setError(data.error);
           setSuccess(data.success);
         })
     })
-    // if we didn't want to do the server action then we could do in such a way 
-    // axios.post("/your/api/route",values)
   }
 
   return (
     <CardWrapper
-      headerLabel="Welcome back"
-      backButtonLabel="Don't have an account?"
-      backButtonHref="/auth/register"
+      headerLabel="Create an account"
+      backButtonLabel="Already have an account?"
+      backButtonHref="/auth/login"
       showSocial
     >
       <Form {...form}>
@@ -66,6 +62,23 @@ export const LoginForm = () => {
             className="space-y-6"    
         >
             <div className="space-y-4">
+            <FormField 
+                    control={form.control}
+                    name="name"
+                    render={({field})=>(
+                        <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                                <Input 
+                                {...field}
+                                disabled={isPending}
+                                placeholder="Nishant Thapa"
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
                 <FormField 
                     control={form.control}
                     name="email"
@@ -80,11 +93,6 @@ export const LoginForm = () => {
                                 type="email"
                                 />
                             </FormControl>
-                            {/* Message of the form , we can also change this message by going in the Login Schema*/
-                            // email:z.string().email({
-                            //  message:"Email is Required"
-                            // })
-                           }
                             <FormMessage />
                         </FormItem>
                     )}
@@ -103,28 +111,19 @@ export const LoginForm = () => {
                                 disabled={isPending}
                                 />
                             </FormControl>
-                            {/* Message of the form , we can also change this message by going in the Login Schema*/
-                            // email:z.string().email({
-                            //  message:"Email is Required"
-                            // })
-                           }
                             <FormMessage />
                         </FormItem>
                     )}
                 />
             </div>
-            {/* only one of the message will be visible */}
-            {/* <FormError message="Something went Wrong"/> */}
-            {/* <FormSuccess message="Email Sent"/> */}
             <FormError message={error}/>
             <FormSuccess message={success}/>
-            {/* Button component */}
             <Button
                 type="submit"
                 className="w-full"
                 disabled={isPending}
             >
-              Login      
+              Create an Account   
             </Button>
         </form>
       </Form>
--- schemas/index.tsx
+++ schemas/index.tsx
@@ -1,13 +1,25 @@
 import * as z from "zod";
 
 export const LoginSchema = z.object({
-    email: z.string().email({
-      message:"Email is required"
-    }),
-    password: z.string().min(1,{
-      message:"Password is required" 
-      // Here default message is String must contain at least 1 character this should not be the message since the password is already decided by the users. 
-    }),
-    // for registering new users we will also add the minimum length of 6 but we should keep it in login since it is possible before we keep min(6) there would have been users who created the password of length less than 6
-    // 
-  }); 
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string().min(1, {
+    message: "Password is required",
+    // Here default message is String must contain at least 1 character this should not be the message since the password is already decided by the users.
+  }),
+  // for registering new users we will also add the minimum length of 6 but we should keep it in login since it is possible before we keep min(6) there would have been users who created the password of length less than 6
+  //
+});
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string().min(6, {
+    message: "Minimum 6 character required",
+  }),
+  name:z.string().min(1,{
+    message:"Name is required"
+  }),
+});
